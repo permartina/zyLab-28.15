a811e2200c00cb19d171e0892f9213ca3f688210
permartina <152039349+permartina@users.noreply.github.com>
2023-12-07 22:22:57 -0600

    Adding PlaylistNode.cpp file
    Started the playlistnode.cpp file with the constructor

--- main.cpp
+++ main.cpp
@@ -1,2 +1,16 @@
 #include <iostream>
 using namespace  std;
+
+
+
+// PlaylistNode.cpp FILE
+
+#include "PlaylistNode.h"  
+using namespace std;
+PlaylistNode::PlaylistNode() : uniqueID("none"), songName("none"), artistName("none"), songLength(0), nextNodePtr(nullptr) {} 
+
+PlaylistNode::PlaylistNode(string id, string name, string artist, int length) 
+
+    : uniqueID(id), songName(name), artistName(artist), songLength(length), nextNodePtr(nullptr) {} 
+
+  
